8249bc426ebcfca6477ecbfc3baffbfb1c26b6e9
nina <ninazugaj25@gmail.com>
2026-03-05 11:30:05 +0100

    Add second test

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -13,6 +13,7 @@ import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class TestJobs2dPatterns {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -27,6 +28,9 @@ public class TestJobs2dPatterns {
 				DriverFeature.getDriverManager());
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+
+		application.addTest("Figure Joe 2", (ActionEvent e) ->
+				FiguresJoe.figureScript2(DriverFeature.getDriverManager().getCurrentDriver()));
 	}
 
 	/**
